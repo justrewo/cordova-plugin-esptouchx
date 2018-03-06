642d350128876a53fe9ef01d1447ef1d605f1777
justwangjun <forever1984@163.com>
2018-03-06 16:53:00 +0800

    update code

--- www/esptouch.js
+++ www/esptouch.js
@@ -1,7 +1,7 @@
 var exec = require('cordova/exec');
 
 module.exports = {
-  start: function (apSsid, apBssid, apPassword, isSsidHiddenStr, taskResultCountStr, successCallback, failCallback) {
+  start: function (apSsid, apBssid, apLocalIP, apPassword, isSsidHiddenStr, taskResultCountStr, successCallback, failCallback) {
     exec(successCallback, failCallback, "esptouch", "start", [apSsid, apBssid, apLocalIP, apPassword, isSsidHiddenStr, taskResultCountStr]);
   },
   stop: function (successCallback, failCallback) {
